377a3df703e5658d2f6100ba6162286ac19fbf87
junell1453 <projecte2@163.com>
2025-10-24 00:33:33 +0800

    mm启用

---
+++ CODE笔记.mm
@@ -0,0 +1,252 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_874441607" name="根节点" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="CODE笔记" FOLDED="false" ID="ID_874441607" CREATED="1761200221034" MODIFIED="1761225778490" LINK="../yuan/todo-sunny.mm#ID_874441607">
+<edge STYLE="bezier" COLOR="#0000ff" WIDTH="1" DASH="SOLID"/>
+<hook NAME="MapStyle" background="#cddec2ff" zoom="1.4641001">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1471964395" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1471964395" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="76" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<node TEXT="门电路" POSITION="bottom_or_right" ID="ID_590030202" CREATED="1761226237735" MODIFIED="1761226242576">
+<edge COLOR="#007c00"/>
+<node TEXT="NAND" POSITION="bottom_or_right" ID="ID_1890268076" CREATED="1761230566198" MODIFIED="1761231736279"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      与非
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="\latex A NAND B=$\overline{A\cdot B}$" ID="ID_982342688" CREATED="1761230619419" MODIFIED="1761231740600"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      全1才0,AND反
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="函数完备" ID="ID_624916878" CREATED="1761231758755" MODIFIED="1761231781344">
+<node TEXT="\latex $\overline{A}$=A  NAND  A" ID="ID_1104467914" CREATED="1761229571509" MODIFIED="1761234649135"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NOT
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="\latex $A\cdot B=\overline{A\   \text{NAND}\   B}$" ID="ID_474762019" CREATED="1761231782463" MODIFIED="1761234840100"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AND
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="\latex A+B=$\overline{A}$ NAND $\overline{B}$" ID="ID_325430235" CREATED="1761235090588" MODIFIED="1761235303171"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      OR
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="NOR" POSITION="bottom_or_right" ID="ID_378731955" CREATED="1761200272872" MODIFIED="1761231748100"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      异或
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="\latex A NOR B=$\overline{A}\cdot\overline{B}&#xa;=\overline{A+B}$" ID="ID_1433127534" CREATED="1761226309850" MODIFIED="1761231752332"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      全0才1,OR反
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="函数完备" ID="ID_1451836093" CREATED="1761226213002" MODIFIED="1761230650181"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      universal
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="\latex $\overline{A}$=A  NOR  A" ID="ID_265218899" CREATED="1761229571509" MODIFIED="1761229868583"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NOT
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="\latex $A\cdot B=\overline{A}\   \text{NOR}\   \overline{B}$" ID="ID_344799371" CREATED="1761229743763" MODIFIED="1761231973343"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AND
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="\latex A+B=$\overline{\overline{A+B}}&#xa;=\overline{\text(A \   NOR \   B)}$" ID="ID_1539319269" CREATED="1761230312238" MODIFIED="1761230557707"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      OR
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="布尔代数" POSITION="top_or_left" ID="ID_1830821515" CREATED="1761200328479" MODIFIED="1761225795425">
+<edge COLOR="#00007c"/>
+<node TEXT="德摩根定律" ID="ID_597678673" CREATED="1761225819564" MODIFIED="1761226038626">
+<node TEXT="\latex $\overline{A+B}=\overline{A} \cdot \overline{B}$" ID="ID_77707302" CREATED="1761225841416" MODIFIED="1761226082057"/>
+<node TEXT="\latex $\overline{A \cdot B}=\overline{A }+\overline{B}$" ID="ID_1225402127" CREATED="1761236752412" MODIFIED="1761236820323"/>
+</node>
+<node TEXT="幂等律" ID="ID_1915420578" CREATED="1761236055455" MODIFIED="1761236183521"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      归并重复
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="A+A=A" POSITION="top_or_left" ID="ID_529323256" CREATED="1761231505363" MODIFIED="1761235998881"/>
+<node TEXT="\latex A$\cdot$A=A" POSITION="top_or_left" ID="ID_1875563048" CREATED="1761236071870" MODIFIED="1761236098141"/>
+</node>
+</node>
+<node TEXT="待整理" POSITION="top_or_left" ID="ID_248082131" CREATED="1761235723750" MODIFIED="1761235730335">
+<edge COLOR="#007c7c"/>
+<node TEXT="补码" POSITION="top_or_left" ID="ID_1379291245" CREATED="1761200250192" MODIFIED="1761235739345">
+<node TEXT="用加法来代替减法" ID="ID_140584548" CREATED="1761226610412" MODIFIED="1761226673371"/>
+</node>
+<node TEXT="电路构成" POSITION="top_or_left" ID="ID_1078313155" CREATED="1761235921753" MODIFIED="1761235926369">
+<node TEXT="开关运算" POSITION="top_or_left" ID="ID_597418984" CREATED="1761232066139" MODIFIED="1761236933162">
+<node TEXT="A并B=A+B" ID="ID_341639215" CREATED="1761232094293" MODIFIED="1761236980503"/>
+<node TEXT="\latex A串B=A$\cdot$B" ID="ID_884982241" CREATED="1761232127127" MODIFIED="1761236984163"/>
+</node>
+<node TEXT="电路简化" POSITION="top_or_left" ID="ID_1854690842" CREATED="1761235853131" MODIFIED="1761235857793">
+<node TEXT="" ID="ID_219383091" CREATED="1761235912611" MODIFIED="1761235912611"/>
+</node>
+<node TEXT="继电器到逻辑门" POSITION="top_or_left" ID="ID_1491214990" CREATED="1761200258311" MODIFIED="1761235742593"/>
+</node>
+</node>
+</node>
+</map>
